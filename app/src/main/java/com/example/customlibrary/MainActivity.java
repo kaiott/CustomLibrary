16288f6bd068152c4de1eada460bdef040d06540
Kai Ott <ottka@ethz.ch>
2021-03-31 10:20:26 +0200

    change group to gitlab

--- app/src/main/java/com/example/customlibrary/MainActivity.java
+++ app/src/main/java/com/example/customlibrary/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             Timber.plant(new Timber.DebugTree());
         }
 
+
         double[] data = {1d,23.44,556,7d,.65,.4,43d,3.43,3.32,3d,32d,.3,4d,.34,432d,.4};
         double[] data2 = {3,4,5,64,3,1,2};
         double[] data3 = {0.003,0.04,0.0005,0.05,0.0,0.02,0.0112};
